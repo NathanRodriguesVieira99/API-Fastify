e1873261501e64d84ac0467365dc1f71edeb9119
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-05-06 22:59:49 -0300

    feat(server): update server to use environment variables

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
 import { fastify } from 'fastify';
+import { env } from './env/index.ts';
 
 const server = fastify({});
 
-server.listen({ port: 3333 }).then(() => {
-    console.log('Server is running on PORT 3333');
-});
+server
+    .listen({
+        host: '0.0.0.0',
+        port: env.PORT,
+    })
+    .then(() => {
+        console.log('🚀 Server is running on PORT 3333');
+    });
