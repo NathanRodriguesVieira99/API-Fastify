4e3a56a8b24e649d1d800da94c6c130991042f24
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-05-08 17:28:15 -0300

    feat(server): add routes register and global error handling

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import { fastify } from 'fastify';
 import { env } from './env/index.ts';
+import { Routes } from './http/routes/routes.ts';
+import { ZodError } from 'zod';
 
+// criação do servidor
 const server = fastify({});
 
 server
@@ -11,3 +14,22 @@ server
     .then(() => {
         console.log('🚀 Server is running on PORT 3333');
     });
+
+// instancia das rotas
+server.register(Routes);
+
+// error handler global
+server.setErrorHandler((error, _, reply) => {
+    if (error instanceof ZodError) {
+        return reply
+            .status(400)
+            .send({ message: 'validation error', issues: error.format });
+    }
+
+    if (env.NODE_ENV !== 'production') {
+        console.error(error);
+    } else {
+        // TODO criar log para uma ferramenta externa
+    }
+    return reply.status(500).send({ message: 'Internal server error' });
+});
